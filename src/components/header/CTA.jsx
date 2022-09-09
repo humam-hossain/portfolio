e79a61992a092af18d1f21d1d5d38dcd8b2f93fa
humam-hossain <humam.hossain.e@gmail.com>
2022-09-10 00:53:21 +0600

    basic setup

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Resume from "../../assets/cv.pdf"
+
+const CTA = () => {
+  return <div className="cta">
+      <a href={Resume} download className="btn">Resume</a>
+      <a href="#contact" className="btn btn-primary">Let's Talk</a>
+  </div>;
+};
+
+export default CTA;
